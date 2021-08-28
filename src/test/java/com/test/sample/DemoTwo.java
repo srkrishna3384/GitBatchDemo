98b90049a04831100914847aa882edaac65839cd
srkrishna3384 <srkrishna3384@gmail.com>
2021-08-28 11:36:11 +0530

    added testdemotwo file

---
+++ src/test/java/com/test/sample/DemoTwo.java
@@ -0,0 +1,14 @@
+package com.test.sample;
+
+import org.testng.annotations.Test;
+
+public class DemoTwo {
+	@Test
+	public static void test() {
+		System.out.println("DEMOTWO:WELCOME TO RAMESHSOFT");
+		System.out.println("DEMOTWO:WELCOME TO RAMESHSOFT");
+		System.out.println("DEMOTWO:WELCOME TO RAMESHSOFT");
+		System.out.println("DEMOTWO:WELCOME TO RAMESHSOFT");
+
+	}
+}
