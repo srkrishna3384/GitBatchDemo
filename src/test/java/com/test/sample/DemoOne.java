f683abd00ff295a80a6b1852d36f26eecdd23694
srkrishna3384 <srkrishna3384@gmail.com>
2021-08-29 10:26:40 +0530

    Person1 changes in DemoTwo file

--- src/test/java/com/test/sample/DemoOne.java
+++ src/test/java/com/test/sample/DemoOne.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 public class DemoOne {
 	@Test
 	public static void demo() {
+		
 		System.out.println("WELCOME TO RAMESHSOFT");
 		System.out.println("WELCOME TO RAMESHSOFT");
 		System.out.println("WELCOME TO RAMESHSOFT");
--- src/test/java/com/test/sample/DemoTwo.java
+++ src/test/java/com/test/sample/DemoTwo.java
@@ -9,6 +9,8 @@ public class DemoTwo {
 		System.out.println("DEMOTWO:WELCOME TO RAMESHSOFT");
 		System.out.println("DEMOTWO:WELCOME TO RAMESHSOFT");
 		System.out.println("DEMOTWO:WELCOME TO RAMESHSOFT");
+		System.out.println("PERSON1CHANGES:DEMOTWO:WELCOME TO RAMESHSOFT");
+		System.out.println("PERSON1CHANGES:DEMOTWO:WELCOME TO RAMESHSOFT");
 
 	}
 }
